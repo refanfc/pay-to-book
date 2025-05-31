76fa1659593df2ae2cf9c1d9e6fe8721e02b1ec8
refan <37445483+refanfc@users.noreply.github.com>
2025-05-31 19:42:16 +0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,17 @@
 
+// app.tsx
+import { DynamicWidget, DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV!,
+        projectId: process.env.NEXT_PUBLIC_DYNAMIC_PROJECT_ID!,
+        walletConnectors: ['metamask', 'walletconnect'],
+      }}
+    >
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  );
+}
